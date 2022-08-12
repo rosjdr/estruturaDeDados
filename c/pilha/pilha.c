a78088dbc92d2d42324e858a2a80699c9919291b
Trem de Programador <rosjdr@gmail.com>
2022-08-12 13:41:33 -0300

    Definição de pilha

--- c/pilha/pilha.c
+++ c/pilha/pilha.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAMANHO_PILHA 100 // tamanho da pilha
+
+struct pilha {
+    int topo;
+    int elementos[TAMANHO_PILHA];
+};
+
 int main(){
     printf("Hello world!\n");
 }
