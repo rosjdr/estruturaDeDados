7039913270df98fe628a9e78bbe59805dca6f344
Trem de Programador <rosjdr@gmail.com>
2022-09-02 15:17:17 -0300

    Implementação de função para mostrar filmes por ano

--- c/arquivos/filmes.c
+++ c/arquivos/filmes.c
@@ -22,6 +22,7 @@ typedef struct lista_filmes banco_de_dados;
 FILE *abrir_arquivo(char nome[], char modo[]);
 void carregar_filmes(banco_de_dados *banco, FILE *arquivo);
 void mostra_filmes(banco_de_dados *bd);
+void mostra_filmes_por_ano(banco_de_dados *bd);
 
 int main(){
     FILE *arquivo; 
@@ -33,14 +34,12 @@ int main(){
     bd.total_filmes = 0;
     carregar_filmes(&bd, arquivo);
 
-
-
     do {
         printf("\n\n====MENU DE OPCOES====\n");
-        printf("1. Mostrar filmes em ordem alfabetica\n");
+        printf("1. Mostrar filmes em ordem de cadastro\n");
         printf("2. Mostrar filmes em ordem cronologica\n");
-        printf("3. Mostrar filmes por ano\n");
-        printf("4. Localizar filme por titulo\n");
+        printf("3. Pesquisar filmes por ano\n");
+        printf("4. Pesquisar filme por titulo\n");
         printf("5. Sair\n");
         printf("OPCAO: ");
         scanf("%d", &op);
@@ -49,6 +48,9 @@ int main(){
         {
         case 1:
             mostra_filmes(&bd);
+            break;
+        case 3:
+            mostra_filmes_por_ano(&bd);
             break;
         
         default:
@@ -62,10 +64,32 @@ int main(){
     return 0;
 }
 
+void mostra_filmes_por_ano(banco_de_dados *bd){
+    int ano_busca;
+    int filme_encontrado = 0;
+    printf("%s", "INFORME O ANO DE BUSCA: ");
+    scanf("%d", &ano_busca);
+    for(int i=0; i<bd->total_filmes; i++)
+        if(bd->filmes[i].ano == ano_busca){
+            filme_encontrado++;
+            break;
+        }
+    if(filme_encontrado){
+        printf("===== LISTA DE FILMES POR ANO =====\n\n");
+        for(int i=0; i < bd->total_filmes; i++){
+            if(bd->filmes[i].ano == ano_busca)
+                printf("%d - %s - %d\n", bd->filmes[i].id, bd->filmes[i].nome, bd->filmes[i].ano);
+        }
+    } else {
+        printf("NAO FORAM ENCONTRADOS FILMES PARA ESTE ANO!\n\n");
+    }
+
+}
+
 void mostra_filmes(banco_de_dados *bd){
     printf("===== LISTA DE FILMES =====\n\n");
     for(int i=0; i < bd->total_filmes; i++){
-        printf("%d - %s\n", bd->filmes[i].id, bd->filmes[i].nome);
+        printf("%d - %s - %d\n", bd->filmes[i].id, bd->filmes[i].nome, bd->filmes[i].ano);
     }
 }
 
@@ -74,10 +98,16 @@ void carregar_filmes(banco_de_dados *banco, FILE *arquivo){
     filme novo;
     
     while(!feof(arquivo)){
+        char ano_filme[5] = "";
+        char nome_filme[200] = "";
         fgets(linha, 200, arquivo);
         novo.id = banco->total_filmes+1;
-        strcpy(novo.nome,linha);
-        novo.nome[strlen(novo.nome)-1] = 0;
+        if(linha[strlen(linha)-1] == '\n')
+            linha[strlen(linha)-1] = 0;
+        strncpy(ano_filme, linha+strlen(linha)-5, 4);
+        novo.ano = atoi(ano_filme);
+        strncpy(nome_filme, linha, strlen(linha)-6);
+        strcpy(novo.nome, nome_filme);
         banco->filmes[banco->total_filmes++] = novo;
     }
 }
