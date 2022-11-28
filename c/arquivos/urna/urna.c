41892a68ed919939e0576f5c0c830ae840b1df1f
Trem de Programador <rosjdr@gmail.com>
2022-11-28 10:53:52 -0300

    Implementação do sistema de urnas

---
+++ c/arquivos/urna/urna.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+int main(){
+
+    system("cls");
+    printf("%s", "=== SISTEMA DE VOTACAO - MENU PRINCIPAL === \n\n");
+    printf("%s", "1. Candidatos\n");
+    printf("%s", "2. Relatorio de candidatos\n");    
+    printf("%s", "3. Iniciar votacao\n");
+    printf("%s", "4. Apurar resultado\n");    
+    printf("%s", "5. Sair\n\n");    
+    printf("%s", "OPCAO: ");    
+    
+    return 0;
+}
